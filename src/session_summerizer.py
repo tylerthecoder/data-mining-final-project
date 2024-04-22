e2286f76dcc930ff0bb6748cc125688f8532d06b
Phillip Cannon <phillip.alan.cannon@gmail.com>
2024-04-22 15:18:37 -0500

    rm all the extraneous features

--- src/session_summerizer.py
+++ src/session_summerizer.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import kurtosis
 from itertools import combinations
 
 
@@ -11,11 +10,6 @@ def calculate_entropy(column):
     probabilities = column.value_counts(normalize=True)
     entropy = -np.sum(probabilities * np.log2(probabilities))
     return entropy
-
-def custom_kurtosis(series):
-    """ Calculate kurtosis for a given series, avoiding NaN values """
-    return kurtosis(series, fisher=True, nan_policy='omit')  # 'fisher=True' gives the excess kurtosis
-
 
 def get_specific_time(series, n):
     """ Returns the nth element of a series if it exists, otherwise returns NaN """
@@ -57,13 +51,6 @@ def get_summerized_session_data(use_cache=True):
         median_secs='median',     # Median of seconds
         sd_secs='std',            # Standard deviation of seconds
         skewness_secs='skew',     # Skewness of seconds
-        kurtosis_secs=custom_kurtosis,       # Kurtosis of seconds (if added via scipy)
-        first_secs=(lambda x: get_specific_time(x, 0)),   # First action seconds
-        second_secs=(lambda x: get_specific_time(x, 1)),  # Second action seconds
-        last_secs=(lambda x: get_specific_time(x, -1)),   # Last action seconds
-        second_last_secs=(lambda x: get_specific_time(x, -2)),  # Second-to-last action seconds
-        third_last_secs=(lambda x: get_specific_time(x, -3))    # Third-to-last action seconds
-    
     ).reset_index()
 
 
@@ -80,33 +67,6 @@ def get_summerized_session_data(use_cache=True):
         'device_type': 'unique_device_types'
     })
 
-
-    # Calculate entropies per user
-    entropy_df = sessions_df.groupby('user_id').agg({
-        'action': lambda x: calculate_entropy(x),
-        'action_type': lambda x: calculate_entropy(x),
-        'action_detail': lambda x: calculate_entropy(x),
-        'device_type': lambda x: calculate_entropy(x)
-    }).reset_index().rename(columns={
-        'action': 'action_entropy',
-        'action_type': 'action_type_entropy',
-        'action_detail': 'action_detail_entropy',
-        'device_type': 'device_type_entropy'
-    })
-
-    # Calculate pairwise ratios of unique counts
-    columns = ['unique_actions', 'unique_action_types', 'unique_action_details', 'unique_device_types']
-    entropy_columns = ['action_entropy', 'action_type_entropy', 'action_detail_entropy', 'device_type_entropy']
-    
-    # Calculate pairwise ratios for unique counts and entropy
-    for (col1, col2) in combinations(columns, 2):
-        unique_counts_df[f'{col1}_to_{col2}_ratio'] = unique_counts_df[col1] / unique_counts_df[col2]
-
-    for (col1, col2) in combinations(entropy_columns, 2):
-        entropy_df[f'{col1}_to_{col2}_ratio'] = entropy_df[col1] / entropy_df[col2]
-
-    # Merge all the dataframes
-    aggregated_df = pd.merge(aggregated_df, entropy_df, on='user_id', how='left')
     aggregated_df = pd.merge(aggregated_df, row_count_df, on='user_id', how='left')
     aggregated_df = pd.merge(aggregated_df, action_present_df, on='user_id', how='left')
     aggregated_df = pd.merge(aggregated_df, unique_counts_df, on='user_id', how='left')
