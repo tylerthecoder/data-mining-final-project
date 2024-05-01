be9324d5d08590080fb93a7635a92ba2600b1cb4
Tyler Tracy <tylertracy1999@gmail.com>
2024-04-30 22:21:02 -0500

    More submission work

--- src/train.py
+++ src/train.py
@@ -97,22 +97,11 @@ if should_grid: # Set true to run the grid search
 
     print("Results sorted by default scoring (accuracy) and saved to 'cv_results.csv'.")
 
-    # Assuming you have already performed the grid search and obtained the results
-    df = pd.DataFrame(model.cv_results_)
-
-    # Sort the DataFrame by the mean test score for the default scoring metric, typically 'mean_test_score'
-    df = df.sort_values('rank_test_score')  # Sorting by rank which is directly related to the default score
-
-    # Save the sorted DataFrame to a CSV file
-    df.to_csv("cv_results.csv", index=False)
-
-    print("Results sorted by default scoring (accuracy) and saved to 'cv_results.csv'.")
-    print(x)
-
 else:
     print("Fitting model...")
     model.fit(X_train, y_train)
     # model.fit(X_train, y_train, eval_set=[(X_test, y_test)], verbose=True)
+
 
 print("Model fitted, predicting...")
 predictions = model.predict(X_test)
@@ -146,10 +135,13 @@ print("SCORE: ", val)
 
 
 
+
 print("Predict on test data")
 df_test = pd.read_csv('data/csv/test_users.csv')
 df_test = process_data(df_test)
-test_preds = model.predict(df_test)
+df_test = df_test.drop('is_weibo', axis=1)
+test_preds = model.predict_proba(df_test)
+test_preds = pd.DataFrame(test_preds, index=df_test.index)
 
 class_dict = {
     'NDF': 0,
@@ -180,3 +172,12 @@ test_preds['get_top'] = test_preds.apply(get_top, axis=1)
 import ast
 test_preds['get_top'] = test_preds['get_top'].apply(lambda x: ast.literal_eval(x))
 print(test_preds.head())
+
+s = test_preds.apply(lambda x: pd.Series(x['get_top']),axis=1).stack().reset_index(level=1, drop=True)
+s.name = 'country'
+
+submission = test_preds.drop([i for i in range(0,12)] + ['get_top'], axis=1).join(s)
+submission.head()
+
+submission.to_csv('submission.csv')
+
